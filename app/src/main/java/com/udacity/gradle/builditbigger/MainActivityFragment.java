793bdb9a591505c40afcdedc1bfd950487b24501
tarun0 <tarun.mudgal34@gmail.com>
2016-06-08 18:45:36 +0530

    GCE config in progress

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,17 +1,28 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.JavaJoke;
 import com.example.androidlib.MainJokeActivity;
+import com.example.backend.myApi.MyApi;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 
 /**
@@ -37,11 +48,14 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 //tellJoke();
-                    Intent intent = new Intent(getActivity().getApplicationContext(), MainJokeActivity.class);
-                    intent.putExtra("joke", joke.getJoke());
-                    startActivity(intent);
+                Intent intent = new Intent(getActivity().getApplicationContext(), MainJokeActivity.class);
+                intent.putExtra("joke", joke.getJoke());
+                new EndpointsAsyncTask().execute(new Pair<Context, String>(getActivity()
+                        .getApplicationContext(), "Manfred"));
+               // startActivity(intent);
             }
         });
+
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
@@ -54,9 +68,44 @@ public class MainActivityFragment extends Fragment {
         return root;
     }
 
-    /*public void tellJoke() {
-        Toast.makeText(getActivity().getApplicationContext(),
-                joke.getJoke(),
-                Toast.LENGTH_SHORT).show();
-    }*/
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private  MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
